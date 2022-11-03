8fa0e6d8df52f48a358a74223add6a3a36805531
KDH <jetmax68@gmail.com>
2022-11-04 01:06:16 +0900

    Seupi Particle Timeline Setting

---
+++ Assets/TimeLineManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public enum TimeLine
+{
+	Test
+}
+public class TimeLineManager : MonoBehaviour
+{
+	[SerializeField]
+	private TimelinePlayable[] _timelines;
+}
