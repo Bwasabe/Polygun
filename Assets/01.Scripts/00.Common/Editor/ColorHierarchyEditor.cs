4a9f2e2a339d0e58834be7afe14f1e62aff3fd20
Bwasabe <77600992+Bwasabe@users.noreply.github.com>
2022-11-03 15:42:20 +0900

    Initial commit

---
+++ Assets/01.Scripts/00.Common/Editor/ColorHierarchyEditor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ColorHierarchyEditor
+{
+    [MenuItem("ColorHierarchy/AddColorHierarchy %H")]
+    [MenuItem("GameObject/ColorHierarchy/AddColorHierarchy %H")]
+    private static void CreateColorHierarchy()
+    {
+        GameObject[] obj = Selection.gameObjects;
+
+
+        for (int i = 0; i < obj.Length; i++)
+        {
+            Undo.AddComponent<ColorHierarchy>(obj[i]);
+        }
+    }
+
+    [MenuItem("ColorHierarchy/RemoveColorHierarchy %#H")]
+    [MenuItem("GameObject/ColorHierarchy/AddColorHierarchy %#H")]
+    private static void RemoveColorHierarchy()
+    {
+        GameObject[] obj = Selection.gameObjects;
+
+
+        for (int i = 0; i < obj.Length; i++)
+        {
+            ColorHierarchy ch = obj[i].GetComponent<ColorHierarchy>();
+            if(ch != null){
+                Undo.DestroyObjectImmediate(ch);
+            }
+        }
+    }
+}
