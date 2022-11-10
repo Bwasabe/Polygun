fc563cdaa3af68e99ccd0933decf8845d8f10b42
KDH <jetmax68@gmail.com>
2022-11-10 14:18:10 +0900

    파티클

--- Assets/01.Scripts/08. Seupi/PlayerFollow.cs
+++ Assets/01.Scripts/08. Seupi/PlayerFollow.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class PlayerFollow : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject Player; //이거 나중에 게임매니져에서 가져오기
-
+ //이거 나중에 게임매니져에서 가져오기
 	[SerializeField]
 	private float speed;
 	private void Update()
@@ -17,6 +15,6 @@ public class PlayerFollow : MonoBehaviour
 	private void Follow()
 	{
 		this.transform.position = Vector3.Slerp
-			(this.transform.position, Player.transform.position, Time.deltaTime*speed);
+			(this.transform.position, GameManager.Instance.Player.transform.position, Time.deltaTime*speed);
 	}
 }
