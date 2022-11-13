adb09ffe06562dbe8eb607511fd1b35eafbbccd1
Bwasabe <jixg20@gmail.com>
2022-11-13 21:00:08 +0900

    Rotation 변경

--- Assets/01.Scripts/06.Player/PlayerRotation.cs
+++ Assets/01.Scripts/06.Player/PlayerRotation.cs
@@ -14,7 +14,8 @@ public class PlayerRotation : BasePlayerComponent
         {
             Vector3 dir = (hit.point - transform.position).normalized;
             dir.y = 0f;
-            transform.rotation = Quaternion.LookRotation(dir*Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.2f);
+            //Quaternion.LookRotation(dir * Time.deltaTime);
         }
     }
 
