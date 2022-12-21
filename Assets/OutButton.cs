74fdfe460df5dcd1172905966d087ede7e255248
KDH <jetmax68@gmail.com>
2022-12-22 01:47:55 +0900

    lobby scene

--- Assets/OutButton.cs
+++ Assets/OutButton.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SettingButton : Btn
+public class OutButton : Btn
 {
 	[SerializeField]
 	private GameObject _obj;
 	public override void Interaction()
 	{
-		_obj.SetActive(true);
+		_obj.SetActive(false);
 	}
 }
