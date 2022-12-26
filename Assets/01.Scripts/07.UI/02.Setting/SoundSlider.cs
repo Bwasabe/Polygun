e47d43a3cf97fc0b8d07aa3876a7ca05e3d0cd5e
Bwasabe <jixg20@gmail.com>
2022-12-26 18:58:37 +0900

    버그 수정

--- Assets/01.Scripts/07.UI/02.Setting/SoundSlider.cs
+++ Assets/01.Scripts/07.UI/02.Setting/SoundSlider.cs
@@ -8,33 +8,37 @@ using UnityEngine.UI;
 public class SoundSlider : MonoBehaviour
 {
     [SerializeField]
-    private List<AudioMixerGroup> _group;
+    private List<string> _group;
 
     private AudioMixer _audioMixer;
 
 
     private Slider _slider;
 
-    private void Awake() {
+    private void Awake()
+    {
         _slider = GetComponent<Slider>();
         _audioMixer = SoundManager.Instance.AudioMixer;
     }
-    
-    private void Start() {
+
+    private void Start()
+    {
         _slider.onValueChanged.AddListener(ChangeVolume);
     }
 
     private void OnEnable() {
-        float value;
-        if(_audioMixer.GetFloat(_group.ToString(), out value))
+        if(_audioMixer.GetFloat(_group[0], out float value))
+        {
             _slider.value = value;
+        }
     }
 
-    public void ChangeVolume(float volume){
+    public void ChangeVolume(float volume)
+    {
         _group.ForEach(g =>
         {
-            if (volume == -40f) _audioMixer.SetFloat(g.ToString(), -80f);
-            else _audioMixer.SetFloat(g.ToString(), volume);
+            if (volume == -40f) _audioMixer.SetFloat(g, -80f);
+            else _audioMixer.SetFloat(g, volume);
         });
     }
 }
